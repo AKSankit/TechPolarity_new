979152503153bbb176e3cea929b58a1025f3ba6a
AKSankit <ankitsingh1757@gmail.com>
2025-03-24 23:11:45 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import HomePage from "./pages/HomePage.jsx";
-import NewsPage from './component/Header/NewsPage.jsx';
+    import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+    import './App.css';
+    import HomePage from "./pages/HomePage.jsx";
+    import NewsPage from './component/Header/NewsPage.jsx';
 
-function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/news" element={<NewsPage />} />
-            </Routes>
-        </Router>
-    );
-}
+    function App() {
+        return (
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/news" element={<NewsPage />} />
+                </Routes>
+            </Router>
+        );
+    }
 
-export default App;
+    export default App;
--- src/component/Articles/TrendingStory.jsx
+++ src/component/Articles/TrendingStory.jsx
@@ -8,7 +8,7 @@ const TrendingStory = () => {
     ];
 
     return (
-        <div className="bg-red-500 p-4 rounded-lg">
+        <div className="bg-red-500 p-2 rounded-lg">
             <h2 className="text-white text-xl font-bold mb-4">Trending Story</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 {stories.map((story, index) => (
@@ -16,7 +16,7 @@ const TrendingStory = () => {
                         <img
                             src={story.imgSrc}
                             alt={story.alt}
-                            className="w-full h-35 object-cover mb-2 rounded-xl"
+                            className="w-full h-35 object-cover mb-2 rounded-xl "
                         />
                         {[...Array(3)].map((_, i) => (
                             <div key={i} className="h-1 bg-white mt-2"></div>
